4be752aed458d7d800dd7c9e3c256023b5e5dc3a
Håvard Ola Eggen <havard.ola.eggen@bekk.no>
2023-06-04 20:07:45 +0200

    Resett konteksten etter databaseendringer.

--- src/test/kotlin/no/bekk/apiworkshop/apiworkshop/oppgaver/Oppgave1.kt
+++ src/test/kotlin/no/bekk/apiworkshop/apiworkshop/oppgaver/Oppgave1.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -49,6 +50,7 @@ class Oppgave1 {
     }
 
     @Test
+    @DirtiesContext
     fun `Legg til en ny bruker`() {
         val name = "Gunde Svan"
         val age = 42

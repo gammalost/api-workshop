1797cbebc51d72ad236194f74918a0530743ff28
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2023-06-13 11:46:06 +0200

    Hello world

--- src/main/kotlin/no/bekk/apiworkshop/apiworkshop/controller/UserController.kt
+++ src/main/kotlin/no/bekk/apiworkshop/apiworkshop/controller/UserController.kt
@@ -1,14 +1,7 @@
 package no.bekk.apiworkshop.apiworkshop.controller
 
 import no.bekk.apiworkshop.apiworkshop.service.UserService
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,34 +10,7 @@ class UserController(
 ) {
 
     @GetMapping("/hello")
-    fun helloWorld() = userService.helloWorld()
-
-    @GetMapping("/users")
-    fun getUsers(
-        @RequestParam alderFra: Int?,
-        @RequestParam alderTil: Int?,
-    ) = userService.getUsers(alderFra, alderTil)
-
-    @GetMapping("/user/{id}")
-    fun getUser(@PathVariable id: String) = userService.getUser(id) ?: ResponseEntity.status(HttpStatus.NOT_FOUND)
-
-    @PostMapping("/user")
-    fun createUser(@RequestParam name: String, @RequestParam age: Int): ResponseEntity<String> {
-        userService.createUser(name, age)
-        return ResponseEntity.status(HttpStatus.CREATED).build()
-    }
-
-    @DeleteMapping("/user")
-    fun deleteUser(@RequestParam name: String) = userService.deleteUser(name)
-
-    @GetMapping("/usersDetailed")
-    fun usersDetailed() = userService.getUsersDetailed()
-
-    data class PostUser(val name: String, val age: Int)
-
-    @PostMapping("/postUser")
-    fun postUser(@RequestBody user: PostUser): ResponseEntity<String> {
-        userService.createUser(user.name, user.age)
-        return ResponseEntity.status(HttpStatus.CREATED).build()
+    fun helloWorld(): String {
+        return userService.helloWorld()
     }
 }

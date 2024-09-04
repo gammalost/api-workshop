24ea3700772b75372b78c7828d9fdc7d2707d9fc
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2024-09-04 10:56:12 +0200

    Test 2.2 skal feile i starten av WS

--- src/test/kotlin/no/bekk/apiworkshop/apiworkshop/oppgaver/Oppgave2.kt
+++ src/test/kotlin/no/bekk/apiworkshop/apiworkshop/oppgaver/Oppgave2.kt
@@ -2,7 +2,6 @@ package no.bekk.apiworkshop.apiworkshop.oppgaver
 
 import no.bekk.apiworkshop.apiworkshop.ApiWorkshopApplication
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,29 +25,34 @@ class Oppgave2 {
     fun `Oppgave 2,1 - Gi 201 Created når man legger til brukere`() {
         val name = "Gunde Svan"
         val age = 42
-        mvc.post("/users") {
-            content = """{"name":"$name","age":$age}"""
-            contentType = MediaType.APPLICATION_JSON
-        }
-            .andExpect { status { isCreated() } }
+        mvc
+            .post("/users") {
+                content = """{"name":"$name","age":$age}"""
+                contentType = MediaType.APPLICATION_JSON
+            }.andExpect { status { isCreated() } }
     }
 
     @Test
     fun `Oppgave 2,2 - Gi 404 Not Found ved henting av brukere som ikke finnes`() {
-        mvc.get("/users/100")
+        mvc
+            .get("/users/1")
+            .andExpect { status { is2xxSuccessful() } }
+        mvc
+            .get("/users/100")
             .andExpect { status { isNotFound() } }
     }
 
     @Test
     fun `Oppgave 2,3 - Returnerer svar eller kaster feil ved deling på null`() {
-        mvc.get("/divide1000by/100")
+        mvc
+            .get("/divide1000by/100")
             .andExpect {
                 status { isOk() }
                 content { json("10") }
             }
 
-
-        mvc.get("/divide1000by/0")
+        mvc
+            .get("/divide1000by/0")
             .andExpect {
                 status { isInternalServerError() }
             }
